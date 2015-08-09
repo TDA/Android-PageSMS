653619a0a7491a84265302b2dc1070fcfd7cd9c9
Sai Pc <schand31@asu.edu>
2015-08-08 21:17:25 -0700

    Initial Commit with empty project

---
+++ src/com/saipc/tda/pageSMS/SMSActivity.java
@@ -0,0 +1,15 @@
+package com.saipc.tda.pageSMS;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SMSActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_SMS);
+    }
+}
