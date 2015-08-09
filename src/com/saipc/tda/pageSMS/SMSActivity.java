a177e2ff1addea1057a17f797fed5236b649c2ed
Sai Pc <schand31@asu.edu>
2015-08-08 21:32:55 -0700

    Linear Layout and attributes added

--- src/com/saipc/tda/pageSMS/SMSActivity.java
+++ src/com/saipc/tda/pageSMS/SMSActivity.java
@@ -10,6 +10,6 @@ public class SMSActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_SMS);
+        setContentView(R.layout.activity_sms);
     }
 }
